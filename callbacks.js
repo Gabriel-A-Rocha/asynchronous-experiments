36849e2c3631e842c88fe8c9368e81616993fb0f
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-05-16 16:09:00 -0400

    feat: create process using callback

---
+++ callbacks.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const a = 3;
+const b = 4;
+
+const c = a * b;
+console.log(c);
+
+fs.readFile("./file.json", (err, data) => {
+  return err ? console.log(err) : console.log(data);
+});
+
+console.log(c);
+
+(doSomething = () => {
+  setTimeout((err) => {
+    return err ? console.log(err) : console.log("doSomething after timeout");
+  }, 1500);
+})();
+
+console.log(c);
+
+/* output:
+12
+12
+12
+fs error
+doSomething after timeout
+*/
